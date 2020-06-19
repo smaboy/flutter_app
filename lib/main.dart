e2c6992626d4cf0fde46bec01f59b4ba4d302454
liyongliang3 <liyongliang3@ceair.com>
2020-06-19 14:32:06 +0800

    实现沉浸式状态栏

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:flutter/services.dart';
 
-void main() => runApp(new MyApp());
+void main() {
+  runApp(new MyApp());
+  if (Platform.isAndroid) {
+// 以下两行 设置android状态栏为透明的沉浸。写在组件渲染之后，是为了在渲染后进行set赋值，覆盖状态栏，写在渲染之前MaterialApp组件会覆盖掉这个值。
+    SystemUiOverlayStyle systemUiOverlayStyle =
+    SystemUiOverlayStyle(statusBarColor: Colors.transparent);
+    SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
+  }
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -22,6 +32,7 @@ class MyApp extends StatelessWidget {
 
     return new MaterialApp(
       title: "Startup Name Generator",
+      theme: new ThemeData(primaryColor: Colors.blueAccent),
       home: new RandomWords(),
     );
   }
