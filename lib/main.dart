55ebc723158fced95551e407cca6d24d598423ec
liyongliang3 <liyongliang3@ceair.com>
2020-06-19 11:25:31 +0800

    1.创建一个无限滚动ListView 2. 添加交互

--- lib/main.dart
+++ lib/main.dart
@@ -6,36 +6,101 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
-    return new MaterialApp(
-      title: 'Welcome to Flutter',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Welcome to Flutter'),
-        ),
-        body: new Center(
-//          child: new Text('Hello World'),
-          child: new RandomWords(),
+//    final wordPair = new WordPair.random();
+//    return new MaterialApp(
+//      title: 'Welcome to Flutter',
+//      home: new Scaffold(
+//        appBar: new AppBar(
+//          title: new Text('Welcome to Flutter'),
+//        ),
+//        body: new Center(
+////          child: new Text('Hello World'),
+//          child: new RandomWords(),
+//        ),
+//      ),
+//    );
 
-        ),
-      ),
+    return new MaterialApp(
+      title: "Startup Name Generator",
+      home: new RandomWords(),
     );
   }
 }
+
 /// 有状态的类
-class RandomWords extends StatefulWidget{
+class RandomWords extends StatefulWidget {
   @override
   createState() => new RandWordsState();
-
 }
 
 /// 为RandomWords提供状态，修改属性
-class RandWordsState extends State<RandomWords>{
+class RandWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+
+  final _biggerFont = const TextStyle(fontSize: 28.0);
+
+  final _saved = new Set<WordPair>();
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
-    return new Text(wordPair.asPascalCase);
+//    final wordPair = new WordPair.random();
+//    return new Text(wordPair.asPascalCase);
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
   }
 
-}
+  /// 此方法构建显示建议单词对的ListView
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      // 对于每个建议的单词对都会调用一次itemBuilder，然后将单词对添加到ListTile行中
+      // 在偶数行，该函数会为单词对添加一个ListTile row.
+      // 在奇数行，该函数会添加一个分割线widget，来分隔相邻的词对。
+      // 注意，在小屏幕上，分割线看起来可能比较吃力。
+      itemBuilder: (context, i) {
+        // 在每一列之前，添加一个1像素高的分隔线widget
+        if (i.isOdd) return new Divider();
 
+        // 语法 "i ~/ 2" 表示i除以2，但返回值是整形（向下取整），比如i为：1, 2, 3, 4, 5
+        // 时，结果为0, 1, 1, 2, 2， 这可以计算出ListView中减去分隔线后的实际单词对数量
+        final index = i ~/ 2;
+        // 如果是建议列表中最后一个单词对
+        if (index >= _suggestions.length) {
+          // ...接着再生成10个单词对，然后添加到建议列表
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  /// 对于每一个单词对，_buildSuggestions函数都会调用一次_buildRow。
+  ///
+  /// 这个函数在ListTile中显示每个新词对，这使您在下一步中可以生成更漂亮的显示行
+  Widget _buildRow(WordPair pair) {
+    final _alreadySaved = _saved.contains(pair);
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: new Icon(
+        _alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: _alreadySaved ? Colors.red : null,
+      ),
+      onTap: (){
+        setState(() {
+          if(_alreadySaved){
+            _saved.remove(pair);
+          }else{
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+}
