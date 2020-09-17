6c88cc5092c05e7dffd03ba257be4f0b36f47a21
liyongliang3 <liyongliang3@ceair.com>
2020-09-17 15:10:19 +0800

    设置banner的样式&解决切换回来banner快速闪屏切换的问题（当组件创建时开启自动播放，销毁时关闭自动切换）

--- lib/home/HomePage.dart
+++ lib/home/HomePage.dart
@@ -22,7 +22,7 @@ class HomePageState extends State<HomePage> {
 
   /// 轮播图数据
   HomeBannerEntity _homeBannerEntity = HomeBannerEntity();
-
+  SwiperController _swiperController;
 
   @override
   Widget build(BuildContext context) {
@@ -33,19 +33,57 @@ class HomePageState extends State<HomePage> {
           centerTitle: true,
         ),
         body: Container(
-          child: Swiper(
-            itemBuilder: (BuildContext context, int index) {
-              return Image.network(
-                homeBannerDataList[index].imagePath,
-                fit: BoxFit.fitWidth,
-              );
-            },
-            itemCount: homeBannerDataList?.length  ?? 0,
-            pagination: SwiperPagination(),
-            control: SwiperControl(),
-            autoplay: true,
-            duration: 1500,
+//          child: Swiper(
+//            itemBuilder: (BuildContext context, int index) {
+//              return Image.network(
+//                homeBannerDataList[index].imagePath,
+//                fit: BoxFit.fitWidth,
+//              );
+//            },
+//            itemCount: homeBannerDataList?.length  ?? 0,
+//            pagination: SwiperPagination(),
+//            control: SwiperControl(),
+//            autoplay: true,
+//            duration: 1500,
+//          ),
+        child:  Swiper(
+          itemCount: homeBannerDataList?.length  ?? 0,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular((10.0)), // 圆角度
+                image: DecorationImage(
+                  image: NetworkImage(homeBannerDataList[index].imagePath),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            );
+          },
+          loop: true,
+          autoplay: false,
+          autoplayDelay: 3000,
+          //触发时是否停止播放
+          autoplayDisableOnInteraction: true,
+          duration: 600,
+          //默认分页按钮
+//        control: SwiperControl(),
+          controller: _swiperController,
+          //默认指示器
+          pagination: SwiperPagination(
+            // SwiperPagination.fraction 数字1/5，默认点
+            builder: DotSwiperPaginationBuilder(size: 6, activeSize: 9),
           ),
+
+          //视图宽度，即显示的item的宽度屏占比
+          viewportFraction: 0.8,
+          //两侧item的缩放比
+          scale: 0.9,
+
+          onTap: (int index) {
+            //点击事件，返回下标
+            print("index-----" + index.toString());
+          },
+        ),
           height: 180.0,
         )
     );
@@ -55,9 +93,19 @@ class HomePageState extends State<HomePage> {
   void initState() {
     super.initState();
 
+    _swiperController = SwiperController();
+
     /// 初始化数据
     initDat();
 
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    _swiperController.stopAutoplay();
+    _swiperController.dispose();
   }
 
   /// 初始化数据
@@ -86,6 +134,8 @@ class HomePageState extends State<HomePage> {
       _homeBannerEntity = homeBannerEntityFromJson(_homeBannerEntity, json.decode(responsesBody));
       print("meBeanEntity转化的第一列的标题为:${_homeBannerEntity.data[0].title}");
     });
+
+    _swiperController.startAutoplay();
 
   }
 }
