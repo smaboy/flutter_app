53f8790855191d88e2c1aeb4bab080cc4cf69e35
liyongliang3 <liyongliang3@ceair.com>
2020-09-24 21:39:13 +0800

    登录页面

--- lib/me/LoginPage.dart
+++ lib/me/LoginPage.dart
@@ -2,34 +2,83 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:toast/toast.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).primaryColor,
       appBar: AppBar(
-        title: Text("登录",style: TextStyle(color: Colors.red,fontSize: 15.0,fontWeight: FontWeight.bold)),
-        actions: <Widget>[
-          RaisedButton(child: Text("注册",style: TextStyle(color: Colors.red,fontSize: 13.0),),onPressed: (){
-            Toast.show("注册", context);
-          },)
-        ],
+        title: Text("登录"),
+        flexibleSpace: Text("hhaha"),
       ),
-      body: Center(
+      body: Container(
+        alignment: Alignment.topCenter,
+        margin: EdgeInsets.all(10.0),
+        padding: EdgeInsets.fromLTRB(10.0, 50.0, 10.0, 0.0),
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-//            TextField(
-//
-//            ),
-//            TextField(
-//
-//            ),
-            Text("登录",style: TextStyle(color: Colors.red,fontSize: 15.0,fontWeight: FontWeight.bold))
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                    labelText: "用户名",
+                    helperText: "用户名只能是英文大小写字母、@、*、_等字符",
+                    prefixIcon: Icon(Icons.person),
+                    contentPadding: EdgeInsets.all(5.0),
+                    border: OutlineInputBorder(
+                        gapPadding: 5.0,
+                        borderRadius: BorderRadius.all(Radius.circular(5.0)))),
+                keyboardType: TextInputType.text,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                    labelText: "密码",
+                    helperText: "密码只能是英文大小写字母、@、*、_等字符",
+                    prefixIcon: Icon(Icons.vpn_key),
+                    suffixIcon: Icon(Icons.lock_open),
+                    contentPadding: EdgeInsets.all(5.0),
+                    border: OutlineInputBorder(
+                        gapPadding: 5.0,
+                        borderRadius: BorderRadius.all(Radius.circular(5.0)))),
+                keyboardType: TextInputType.visiblePassword,
+                obscureText: true,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(20.0),
+              padding: const EdgeInsets.all(8.0),
+              child: DecoratedBox(
+                child: Container(
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.all(10.0),
+                  child: Text(
+                    "登录",
+                  ),
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(3.0),
+                  border: Border.all(color: Colors.red[400], width: 1.0),
+                  color: Colors.white,
+                  shape: BoxShape.rectangle,
+                  gradient: LinearGradient(
+                      colors: [ Colors.green, Colors.blue],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight),
+                ),
+              ),
+            ),
           ],
         ),
       ),
     );
   }
 }
-
-
