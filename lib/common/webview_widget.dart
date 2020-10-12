662c16c47e46352556df0a7ca65b72970c9c9735
liyongliang3 <liyongliang3@ceair.com>
2020-10-12 17:02:59 +0800

    更新(加载弹窗宽高自适应)

--- lib/common/webview_widget.dart
+++ lib/common/webview_widget.dart
@@ -20,7 +20,7 @@ class _WebViewWidgetState extends State<WebViewWidget> {
 
   StreamSubscription<double> _onProgressChanged;
 
-  int _progressValue = 0 ;
+  int _progressValue = 0;
 
   @override
   void initState() {
@@ -28,15 +28,16 @@ class _WebViewWidgetState extends State<WebViewWidget> {
 
     _onProgressChanged =
         flutterWebViewPlugin.onProgressChanged.listen((double progress) {
-          if (mounted) {
-            setState(() {
-              _progressValue = (progress*100).toInt();
-              print("webview当前进度为:progress=$progress");
-              print("webview当前进度为:_progressValue=$_progressValue");
-            });
-          }
+      if (mounted) {
+        setState(() {
+          _progressValue = (progress * 100).toInt();
+          print("webview当前进度为:progress=$progress");
+          print("webview当前进度为:_progressValue=$_progressValue");
         });
+      }
+    });
   }
+
   @override
   void dispose() {
     _onProgressChanged.cancel();
@@ -51,17 +52,25 @@ class _WebViewWidgetState extends State<WebViewWidget> {
       appBar: AppBar(
         title: Text(widget.title),
         centerTitle: true,
-        leading: IconButton(icon: Icon(Icons.arrow_back),onPressed: (){
-          Navigator.pop(context);
-        },),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
         actions: <Widget>[
-          IconButton(icon: Icon(Icons.share), onPressed: () {
-            print("我是分享");
-          },),
-          IconButton(icon: Icon(Icons.more_horiz), onPressed: () {
-            print("我是更多");
-          },),
-
+          IconButton(
+            icon: Icon(Icons.share),
+            onPressed: () {
+              print("我是分享");
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.more_horiz),
+            onPressed: () {
+              print("我是更多");
+            },
+          ),
         ],
       ),
       withZoom: true,
@@ -70,35 +79,38 @@ class _WebViewWidgetState extends State<WebViewWidget> {
       initialChild: Container(
         alignment: Alignment.center,
         color: Colors.grey[400],
-        child: Container(
-          alignment: Alignment.center,
-          padding: EdgeInsets.all(15.0),
-          constraints: BoxConstraints.tightFor(width: 150,height: 150),
+        child: DecoratedBox(
           decoration: BoxDecoration(
-            color: Colors.white,
+              color: Colors.white,
 //            border: Border.all(//边框
 //              color: Colors.blue,
 //              width: 2.0,
 //              style: BorderStyle.solid
 //            ),
-            borderRadius: BorderRadius.all(Radius.circular(5.0)),
-            boxShadow:[
-              BoxShadow(
-                color: Colors.grey,
-                offset: Offset(3.0,3.0),
-                blurRadius: 4.0
-              )
-            ]
-
-
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              CircularProgressIndicator(),
-              Padding(child: Text('加载中$_progressValue%...',textScaleFactor: 0.5,),padding: EdgeInsets.all(10.0),),
-            ],
+              borderRadius: BorderRadius.all(Radius.circular(5.0)),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey,
+                    offset: Offset(3.0, 3.0),
+                    blurRadius: 4.0)
+              ]),
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                CircularProgressIndicator(),
+                Padding(
+                  child: Text(
+                    '加载中$_progressValue%...',
+                    textScaleFactor: 0.5,
+                  ),
+                  padding: EdgeInsets.all(10.0),
+                ),
+              ],
+            ),
           ),
         ),
       ),
