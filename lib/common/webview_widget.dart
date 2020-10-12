0c98615e69a64401c634908a734d0ecf81284c27
liyongliang3 <liyongliang3@ceair.com>
2020-10-12 16:40:42 +0800

    实时显示webview的加载进度

--- lib/common/webview_widget.dart
+++ lib/common/webview_widget.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
@@ -14,6 +16,28 @@ class WebViewWidget extends StatefulWidget {
 }
 
 class _WebViewWidgetState extends State<WebViewWidget> {
+  final flutterWebViewPlugin = FlutterWebviewPlugin();
+
+  StreamSubscription<double> _onProgressChanged;
+
+  int _progressValue = 0 ;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _onProgressChanged =
+        flutterWebViewPlugin.onProgressChanged.listen((double progress) {
+          if (mounted) {
+            setState(() {
+              _progressValue = (progress*100).toInt();
+              print("webview当前进度为:progress=$progress");
+              print("webview当前进度为:_progressValue=$_progressValue");
+            });
+          }
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return WebviewScaffold(
@@ -38,9 +62,38 @@ class _WebViewWidgetState extends State<WebViewWidget> {
       withLocalStorage: true,
       hidden: true,
       initialChild: Container(
-        color: Colors.blueGrey,
-        child: const Center(
-          child: Text('加载中...'),
+        alignment: Alignment.center,
+        color: Colors.grey[400],
+        child: Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.all(15.0),
+          constraints: BoxConstraints.tightFor(width: 150,height: 150),
+          decoration: BoxDecoration(
+            color: Colors.white,
+//            border: Border.all(//边框
+//              color: Colors.blue,
+//              width: 2.0,
+//              style: BorderStyle.solid
+//            ),
+            borderRadius: BorderRadius.all(Radius.circular(5.0)),
+            boxShadow:[
+              BoxShadow(
+                color: Colors.grey,
+                offset: Offset(3.0,3.0),
+                blurRadius: 4.0
+              )
+            ]
+
+
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              CircularProgressIndicator(),
+              Padding(child: Text('加载中$_progressValue%...',textScaleFactor: 0.5,),padding: EdgeInsets.all(10.0),),
+            ],
+          ),
         ),
       ),
     );
