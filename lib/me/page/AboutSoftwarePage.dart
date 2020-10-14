9f55073a54eff2271d79faa30ac459f6bc9a7ad3
liyongliang3 <liyongliang3@ceair.com>
2020-10-14 10:12:33 +0800

    更新关于软件页面&修改android的应用id和应用名

--- lib/me/page/AboutSoftwarePage.dart
+++ lib/me/page/AboutSoftwarePage.dart
@@ -42,6 +42,11 @@ class _AboutSoftwarePageState extends State<AboutSoftwarePage> {
         body: SingleChildScrollView(
           child: Column(
             children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(15.0),
+                child: Text("   该软件采用flutter跨平台框架开发,采用玩安卓平台的开放API实现的功能,基本UI设计保持玩安卓平台样式。该app采用了大量优秀的第三方框架，如用于h5页面的flutter_webview_plugin、"
+                    "用于网络请求的dio、用于下拉刷新的pull_to_refresh等等。该项目的主要目的是学习flutter框架，如有不足之处还请谅解。最后感谢API的提供者鸿洋大神，还有众多的优秀框架的提供者。",textScaleFactor: 0.7,),
+              ),
               ListTile(
 //                title: Text("关于作者: Smaboy"),
                 title: Text.rich(TextSpan( text: "关于作者: ",
@@ -55,9 +60,9 @@ class _AboutSoftwarePageState extends State<AboutSoftwarePage> {
                   RouteHelpUtils.push(context, WebViewWidget(url: "https://github.com/smaboy",title: "Smaboy",));
                 },
               ),
-              Divider(
-                color: Colors.grey,
-              ),
+//              Divider(
+//                color: Colors.grey,
+//              ),
               ListTile(
 //                title: Text("关于项目: flutter_app"),
                 title: Text.rich(TextSpan( text: "关于项目: ",
@@ -71,9 +76,9 @@ class _AboutSoftwarePageState extends State<AboutSoftwarePage> {
                   RouteHelpUtils.push(context, WebViewWidget(url: "https://github.com/smaboy/flutter_app",title: "flutter_app",));
                 },
               ),
-              Divider(
-                color: Colors.grey,
-              ),
+//              Divider(
+//                color: Colors.grey,
+//              ),
               ListTile(
 //                title: Text("关于玩安卓"),
                 title: Text.rich(TextSpan( text: "关于API: ",
@@ -87,9 +92,9 @@ class _AboutSoftwarePageState extends State<AboutSoftwarePage> {
                   RouteHelpUtils.push(context, WebViewWidget(url: "https://www.wanandroid.com/blog/show/2",title: "玩安卓",));
                 },
               ),
-              Divider(
-                color: Colors.grey,
-              ),
+//              Divider(
+//                color: Colors.grey,
+//              ),
             ],
           ),
         ),
