7f9a3bd0d5ebb74551cd7241c4abcdd22bfb86a6
李永良 <18550019225@163.com>
2021-12-31 15:44:59 +0800

    更换webview插件，添加logger、lottie插件

--- lib/common/widget/webview_widget.dart
+++ lib/common/widget/webview_widget.dart
@@ -1,8 +1,6 @@
-import 'dart:async';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 /// webview组件
 class WebViewWidget extends StatefulWidget {
@@ -20,101 +18,25 @@ class WebViewWidget extends StatefulWidget {
 }
 
 class _WebViewWidgetState extends State<WebViewWidget> {
-  final flutterWebViewPlugin = FlutterWebviewPlugin();
-
-  StreamSubscription<double> _onProgressChanged;
-
-  int _progressValue = 0;
-
   @override
   void initState() {
     super.initState();
-
-    _onProgressChanged =
-        flutterWebViewPlugin.onProgressChanged.listen((double progress) {
-      if (mounted) {
-        setState(() {
-          _progressValue = (progress * 100).toInt();
-          print("webview当前进度为:progress=$progress");
-          print("webview当前进度为:_progressValue=$_progressValue");
-        });
-      }
-    });
   }
 
   @override
   void dispose() {
-    _onProgressChanged.cancel();
-
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return WebviewScaffold(
-      url: widget.url,
+    return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
-        centerTitle: true,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-        actions: <Widget>[
-//          Visibility(
-//            visible: widget.showShare,
-//            child: ShareWidget(),
-//          ),
-//          IconButton(
-//            icon: Icon(Icons.more_horiz),
-//            onPressed: () {
-//              print("我是更多");
-//            },
-//          ),
-        ],
+        title: Text(widget.title ?? ''),
       ),
-      withZoom: true,
-      withLocalStorage: true,
-      hidden: true,
-      initialChild: Container(
-        alignment: Alignment.center,
-        color: Colors.grey[400],
-        child: DecoratedBox(
-          decoration: BoxDecoration(
-              color: Colors.white,
-//            border: Border.all(//边框
-//              color: Colors.blue,
-//              width: 2.0,
-//              style: BorderStyle.solid
-//            ),
-              borderRadius: BorderRadius.all(Radius.circular(5.0)),
-              boxShadow: [
-                BoxShadow(
-                    color: Colors.grey,
-                    offset: Offset(3.0, 3.0),
-                    blurRadius: 4.0)
-              ]),
-          child: Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                CircularProgressIndicator(),
-                Padding(
-                  child: Text(
-                    '加载中$_progressValue%...',
-                    textScaleFactor: 0.5,
-                  ),
-                  padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
-                ),
-              ],
-            ),
-          ),
-        ),
+      body: WebView(
+        initialUrl: widget.url ?? '',
+        javascriptMode: JavascriptMode.unrestricted,
       ),
     );
   }
